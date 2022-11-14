0b6b328247e69a5fc56cdeb6565c8c4512c029bb
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2022-11-14 20:50:44 +0200

    Create chapter3.py

---
+++ chapter3 Introducing Lists/chapter3.py
@@ -0,0 +1,46 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles) #just print all list
+print(bicycles[0]) #print one elem(by [index])
+print(bicycles[-1]) #print last elem
+
+
+#Changing, Adding, and Removing Elements
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'#change
+print(motorcycles)
+motorcycles.append('ducati')#Adding to the end of list
+print(motorcycles)
+motorcycles.insert(0, 'ducati')#Adding a new element at any position in your list
+print(motorcycles)
+del motorcycles[0]#Removing by index
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()#Removing an Item Using the pop() Method(last elem)
+print(motorcycles)
+print(popped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop(1)#Popping Items from any Position in a List
+print(motorcycles)
+print(popped_motorcycle)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')#remove by value
+print(motorcycles)
+
+
+#You can also use the remove() method to work with a value that’s being
+#removed from a list. Let’s remove the value 'ducati' and print a reason for
+#removing it from the list:
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
